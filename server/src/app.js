460a1f144fb970d319257096264d8c5882558121
Aditya Jawade <adityajawade93@gmail.com>
2018-03-12 17:16:41 +0530

    add a basic authentication controller with policies

--- server/src/app.js
+++ server/src/app.js
@@ -2,16 +2,18 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const logger = require('morgan')
+const { sequelize } = require('./models')
+const config = require('./config/config')
 
 const app = express()
 app.use(logger('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.post('/register', (req, res) => {
-  res.json({
-    message: `Hello ${req.body.email} new user was registered`
-  })
-})
+require('./routes')(app)
 
-app.listen(process.env.PORT || 8081)
+sequelize.sync()
+  .then(() => {
+    app.listen(config.port)
+    console.log(`server is listening on port ${config.port}`)
+  })
